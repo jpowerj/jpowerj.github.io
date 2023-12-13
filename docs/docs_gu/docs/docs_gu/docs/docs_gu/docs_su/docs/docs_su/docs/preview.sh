a72ed43aa111f84590debb004cfd7d7e4faf6423
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2023-12-13 01:54:22 -0500

    Removed recursive compilation D:

--- docs/docs_gu/docs/docs_gu/docs/docs_gu/docs_su/docs/docs_su/docs/preview.sh
+++
@@ -1,1 +0,0 @@
-bundle exec jekyll serve --livereload --config _dev_config.yml --trace
--- docs/docs_gu/docs_su/docs/docs_gu/docs_su/docs/docs_gu/docs_su/docs/docs_su/build_and_export.sh
+++
@@ -1,6 +0,0 @@
-bundle exec jekyll build --config _gh_config.yml
-bundle exec jekyll build --config _su_config.yml
-bundle exec jekyll build --config _gu_config.yml
-git add -A .
-git commit -m "Auto-commit"
-git push
