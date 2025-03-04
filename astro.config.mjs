2e52dde39984675e11234ece5075ccf146cbd5d3
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2025-03-04 04:43:50 -0500

    Changed astro config site to jjacobs.me

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // Top-Level Options
-  site: 'https://astro-pure.js.org',
+  site: 'https://jjacobs.me',
   // base: '/docs',
   trailingSlash: 'never',
 
