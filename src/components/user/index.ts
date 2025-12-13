6497354cd2f0d72872982f9a133f2ee3ab139964
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2025-12-13 00:06:58 -0500

    Grid layout for courses

--- src/components/user/index.ts
+++ src/components/user/index.ts
@@ -12,7 +12,7 @@ export { default as Timeline } from './Timeline.astro'
 export { default as Steps } from './Steps.astro'
 
 // Simple text rerender
-export { default as Button } from './Button.astro'
+// export { default as Button } from './Button.astro'
 export { default as Spoiler } from './Spoiler.astro'
 export { default as FormattedDate } from './FormattedDate.astro'
 export { default as Label } from './Label.astro'
